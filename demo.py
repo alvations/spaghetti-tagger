1e348fe5e292a24bff26fa290cad5821d06b3398
alvations <alvations@gmail.com>
2016-09-28 19:21:28 +0800

    updated the code

--- demo.py
+++ demo.py
@@ -1,22 +1,38 @@
-import spaghetti as spa
+#-*- coding: utf8 -*-
+from __future__ import print_function
+import spaghetti as sgt
 
-test_sent = 'Mi colega me ayuda a programar cosas .'.split()
-tests = [test_sent, 'Está embarazada .'.split()]
+sent1 = 'Mi colega me ayuda a programar cosas .'.split()
+sent2 = 'Está embarazada .'.split()
+sent3 = '¿ A lo mejor nos acercamos por fin a una odontología sin dolores ?'.split()
+sent4 = '¿ A lo mejor nos acercamos por_fin a una odontología sin dolores ?'.split()
+test_sents = [sent1, sent2, sent3]
 
 # Default Spaghetti tagger.
-print spa.pos_tag(test_sent)
+print (sgt.pos_tag(sent1), end="\n\n")
 
 # Tag multiple sentences.
-print spa.batch_pos_tag(tests)
+print (sgt.pos_tag_sents(test_sents), end="\n\n")
 
-spa_tagger = spa.cesstag()
+spa_tagger = sgt.CESSTagger()
 # POS tagger trained on unigrams of CESS corpus.
-print spa_tagger.uni(test_sent)
+spa_unigram_tagger = spa_tagger.uni
+print (spa_unigram_tagger.tag(sent1))
+print (spa_unigram_tagger.tag(sent4))
 # POS tagger traned on bigrams of CESS corpus.
-print spa_tagger.bi(test_sent)
+spa_bigram_tagger = spa_tagger.bi
+print (spa_bigram_tagger.tag(sent2))
+print (spa_bigram_tagger.tag(sent4))
+print (spa_bigram_tagger.tag_sents(test_sents), end="\n\n")
 
-spa_mwe_tagger = spa.cesstag(mwe=True) # Recognizes Multi-Word Expressions as one token.
+spamwe_tagger = sgt.CESSTagger(use_mwe=True) # Recognizes Multi-Word Expressions as one token.
 # POS tagger trained on unigrams that includes MWEs from the CESS.
-print spa_tagger.uni(test_sent)
+spamwe_unigram_tagger = spamwe_tagger.uni
+print (spamwe_unigram_tagger.tag(sent3))
+print (spamwe_unigram_tagger.tag(sent4))
 # POS tagger trained on bigrams that includes MWEs from the CESS.
-print spa_tagger.bi(test_sent)
+spamwe_bigram_tagger = spamwe_tagger.bi
+print (spamwe_unigram_tagger.tag(sent3))
+print (spamwe_unigram_tagger.tag(sent4))
+print (spamwe_unigram_tagger.tag_sents(test_sents), end="\n\n")
+
--- spaghetti.py
+++ spaghetti.py
@@ -1,69 +1,74 @@
 #-*- coding: utf8 -*-
+from __future__ import print_function
+import sys
 
-from nltk import UnigramTagger as ut
-from nltk import BigramTagger as bt
-from cPickle import dump,load
-		
-def loadtagger(taggerfilename):
-	infile = open(taggerfilename,'rb')
-	tagger = load(infile); infile.close()
-	return tagger
+from nltk import UnigramTagger, BigramTagger
+from nltk.corpus import cess_esp as cess
 
-def traintag(corpusname, corpus):
-	# Function to save tagger.
-	def savetagger(tagfilename,tagger):
-		outfile = open(tagfilename, 'wb')
-		dump(tagger,outfile,-1); outfile.close()
-		return
-	# Training UnigramTagger.
-	uni_tag = ut(corpus)
-	savetagger(corpusname+'_unigram.tagger',uni_tag)
-	# Training BigramTagger.
-	bi_tag = bt(corpus)
-	savetagger(corpusname+'_bigram.tagger',bi_tag)
-	print "Tagger trained with",corpusname,"using" +\
-				"UnigramTagger and BigramTagger."
-	return
-	
-# Function to unchunk corpus.
-def unchunk(corpus):
-	nomwe_corpus = []
-	for i in corpus:
-		nomwe = " ".join([j[0].replace("_"," ") for j in i])
-		nomwe_corpus.append(nomwe.split())
-	return nomwe_corpus
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
-class cesstag():
-	def __init__(self,mwe=True):
-		self.mwe = mwe
-		# Train tagger if it's used for the first time.
-		try:
-			loadtagger('cess_unigram.tagger').tag(['estoy'])
-			loadtagger('cess_bigram.tagger').tag(['estoy'])
-		except IOError:
-			print "*** First-time use of cess tagger ***"
-			print "Training tagger ..."
-			from nltk.corpus import cess_esp as cess
-			cess_sents = cess.tagged_sents()
-			traintag('cess',cess_sents)
-			# Trains the tagger with no MWE.
-			cess_nomwe = unchunk(cess.tagged_sents())
-			tagged_cess_nomwe = batch_pos_tag(cess_nomwe)
-			traintag('cess_nomwe',tagged_cess_nomwe)
-			print
-		# Load tagger.
-		if self.mwe == True:
-			self.uni = loadtagger('cess_unigram.tagger')
-			self.bi = loadtagger('cess_bigram.tagger')
-		elif self.mwe == False:
-			self.uni = loadtagger('cess_nomwe_unigram.tagger')
-			self.bi = loadtagger('cess_nomwe_bigram.tagger')
+def load_tagger(filename):
+    """ Function to load tagger. """
+    with open(filename,'rb') as fin:
+        tagger = pickle.load(fin)
+    return tagger
 
-def pos_tag(tokens, mmwe=True):
-	tagger = cesstag(mmwe)
-	return tagger.uni.tag(tokens)
-	
-def batch_pos_tag(sentences, mmwe=True):
-	tagger = cesstag(mmwe)
-	return tagger.uni.batch_tag(sentences)
+def save_tagger(filename, tagger):
+    """ Function to save tagger. """
+    with open(filename, 'wb') as fout:
+        pickle.dump(tagger, fout)
 
+def train_tagger(corpus_name, corpus):
+    """ Function to train tagger. """
+    # Training UnigramTagger.
+    uni_tag = UnigramTagger(corpus)
+    save_tagger('{}_unigram.tagger'.format(corpus_name), uni_tag)
+    # Training BigramTagger.
+    bi_tag = BigramTagger(corpus, backoff=uni_tag)
+    save_tagger('{}_bigram.tagger'.format(corpus_name), bi_tag)
+    _msg = str("Tagger trained with {} using "
+            "UnigramTagger and BigramTagger.").format(corpus_name)
+    print (_msg, file=sys.stderr)
+
+def unchunk(corpus): 
+    """ Function to unchunk corpus. """
+    nomwe_corpus = []
+    for i in corpus:
+        nomwe = " ".join([j[0].replace("_"," ") for j in i])
+        nomwe_corpus.append(nomwe.split())
+    return nomwe_corpus
+
+
+class CESSTagger():
+    def __init__(self,use_mwe=False):
+        self.use_mwe = use_mwe
+        # Train tagger if it's used for the first time.
+        try:
+            load_tagger('cess_unigram.tagger').tag(['estoy'])
+            load_tagger('cess_bigram.tagger').tag(['estoy'])
+        except IOError:
+            print ("*** First-time use of cess tagger ***", file=sys.stderr)
+            print ("Training tagger ...", file=sys.stderr)
+            # Load CESS corpus.
+            cess_sents = cess.tagged_sents()
+            train_tagger('cess',cess_sents)
+            # Trains the tagger with no MWE.
+            cess_nomwe = unchunk(cess.tagged_sents())
+            tagged_cess_nomwe = pos_tag_sents(cess_nomwe, False)
+            train_tagger('cess_nomwe',tagged_cess_nomwe)
+        # Load tagger.
+        _mwe_option_name = "_nomwe_" if self.use_mwe == True else "_"
+        self.uni = load_tagger('cess{}unigram.tagger'.format(_mwe_option_name))
+        self.bi = load_tagger('cess{}bigram.tagger'.format(_mwe_option_name))
+
+
+def pos_tag(tokens, use_mwe=False):
+    tagger = CESSTagger(use_mwe)
+    return tagger.uni.tag(tokens)
+    
+def pos_tag_sents(sentences, use_mwe=False):
+    tagger = CESSTagger(use_mwe)
+    return tagger.uni.tag_sents(sentences)
